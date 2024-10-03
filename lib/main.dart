c213fc39049cdf32f8132c8c88b37cd121d9da08
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-10-03 14:51:10 +0100

    added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:assessment/auth_manager/auth_manager.dart';
+import 'package:assessment/state_management/auth_bloc.dart';
 import 'package:assessment/view/user_auth/signup_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +15,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: SignupScreen(),
-      );
+    return BlocProvider(
+      create: (context) => AuthBloc(AuthManager(http.Client())),
+      child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: SignupScreen(),
+        ),
+    );
   }
 }
